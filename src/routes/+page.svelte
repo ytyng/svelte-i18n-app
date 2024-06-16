48997bdefc97f0e42cd3a206f8afc7fec4638083
ytyng <ytyng@live.jp>
2024-06-16 21:35:37 +0900

    i18n support

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script lang="ts">
+    import { _ } from 'svelte-i18n'
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+{$_('my.translation.key')}
